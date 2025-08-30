b564a3d4bf3e1345c3faeb50d1149a2dc1156071
RodrigoQF <rodrigo.defrancischi@gmail.com>
2025-08-30 19:31:41 -0300

    Criacao mock response

---
+++ src/app/mocks/cartaoVacinaResponseMock.ts
@@ -0,0 +1,27 @@
+import { CartaoVacinacaoResponse } from "../shared/models/cartaoVacinaModel";
+
+export const MOCK_CARTAO: CartaoVacinacaoResponse = {
+  pessoa: {
+    id: '001',
+    cpf: '12345678900',
+    nome: 'Rodrigo Francischi',
+    idade: 23,
+    sexo: 'Masculino'
+  },
+  vacinas: [
+    {
+      id: 'v1',
+      nome: 'Febre Amarela',
+      data: '2025-01-10',
+      dose: '1ª Dose',
+      fabricante: 'AstraZeneca'
+    },
+        {
+      id: 'v2',
+      nome: 'Covid',
+      data: '2025-01-10',
+      dose: '2ª Dose',
+      fabricante: 'Pfizer'
+    }
+  ]
+};
