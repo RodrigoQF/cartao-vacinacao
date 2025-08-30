913d026bc2a3ddd7d0ccea5ef79fc7fcf2b726e6
RodrigoQF <rodrigo.defrancischi@gmail.com>
2025-08-30 19:32:22 -0300

    Deixando dados dinamicos na tela do cartao de vacinação

--- src/app/modules/cartao-vacina/cartao-vacina-component.ts
+++ src/app/modules/cartao-vacina/cartao-vacina-component.ts
@@ -1,27 +1,63 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Vacina } from '../../shared/models/vacinaModel';
+import { Pessoa } from '../../shared/models/pessoaModel';
+import { CartaoVacinacaoResponse } from '../../shared/models/cartaoVacinaModel';
+import { MOCK_CARTAO } from '../../mocks/cartaoVacinaResponseMock';
 
 @Component({
   selector: 'app-cartao-vacina-component',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './cartao-vacina-component.html',
   styleUrl: './cartao-vacina-component.scss'
 })
 export class CartaoVacinaComponent implements OnInit {
   tipoModal: 'info' | 'add' | null = null;
-
-
+  editandoVacina: boolean = false;
+  cartaoVacina!: CartaoVacinacaoResponse;
+  usuario: Pessoa | null = null;
+  vacinas: Vacina[] = [];
+  possuiVacina: boolean = false;
+  vacinaSelecionada: Vacina = {
+    id: '',
+    nome: '',
+    data: '',
+    dose: '1ª Dose',
+    fabricante: ''
+  };
   ngOnInit(): void {
-    
+    this.cartaoVacina = MOCK_CARTAO;
+    this.possuiVacina = this.cartaoVacina.vacinas.length > 0;
+    this.usuario = this.cartaoVacina.pessoa;
   }
 
-  abrirModal(modal: 'info' | 'add'){
+  abrirModalEditar(modal: 'info' | 'add', vacina: Vacina) {
+    this.vacinaSelecionada = vacina;
     this.tipoModal = modal;
   }
 
-  fecharModal(){
-      this.tipoModal = null;
+  abrirModalAdicionar(modal: 'info' | 'add') {
+    this.tipoModal = modal;
   }
 
+  fecharModal() {
+    this.tipoModal = null;
+    this.editandoVacina = false;
+
+  }
+
+  editarModal() {
+    this.editandoVacina = true;
+  }
+
+  salvarVacina() {
+    this.editandoVacina = true;
+    this.tipoModal = null;
+  }
+
+  excluirVacina(){
+
+  }
 }
