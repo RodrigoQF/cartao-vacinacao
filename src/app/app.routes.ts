06813a412231a7dcb72bbfe95aef751ef5061c1e
RodrigoQF <rodrigo.defrancischi@gmail.com>
2025-08-30 20:19:36 -0300

    Alterando primeiro direcionamento para login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,11 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-    {path: '', pathMatch: 'full', redirectTo: 'cartao-vacina'},
+    {path: '', pathMatch: 'full', redirectTo: 'login'},
+    {
+        path: 'login',
+        loadComponent: () => import('./modules/login-component/login-component').then((m) => m.LoginComponent)
+    },
     {
         path: 'cartao-vacina',
         loadComponent: () => import('./modules/cartao-vacina/cartao-vacina-component').then((m) => m.CartaoVacinaComponent)
