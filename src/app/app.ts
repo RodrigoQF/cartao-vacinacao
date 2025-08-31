59bcbb8855d0508fe5baedc1aaa358a244e4fc47
RodrigoQF <rodrigo.defrancischi@gmail.com>
2025-08-30 21:45:35 -0300

    Implementacao loader compoent e loader service

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,23 @@
-import { Component, signal } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoaderService } from './service/loader.service';
+import { LoaderComponent } from './shared/components/loader-component/loader-component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LoaderComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
-export class App {
+export class App implements OnInit {
   protected readonly title = signal('cartao-vacinacao');
+
+  loader: boolean = true;
+  constructor(private loaderService: LoaderService, private cdr: ChangeDetectorRef){}
+  ngOnInit(): void {
+    this.loaderService.loaderState$.subscribe((state) =>{
+      this.loader = state;
+      this.cdr.detectChanges();
+    })
+  }
 }
